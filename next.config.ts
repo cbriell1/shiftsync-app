3ce6bc1407a792b1e954709ebda28549d25fa46c
Chris Briell <cbriell1@yahoo.com>
2026-03-23 14:06:58 -0400

    fix: force Webpack compiler to resolve Turbopack Prisma module crash

--- next.config.ts
+++ next.config.ts
@@ -5,21 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     // This allows the build to finish even if there are small type errors
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Ignore linting errors during build
-    ignoreDuringBuilds: true,
-  },
-  // Protects heavy backend modules from the Turbopack compiler
-  serverExternalPackages:[
-    "@prisma/client",
-    "@auth/prisma-adapter",
-    "@simplewebauthn/server"
-  ],
-  // FIX: Forcefully packages the Prisma database engine into the Vercel serverless deployment
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/@prisma/client/**/*"],
-    "/api/**/*":["./node_modules/@prisma/client/**/*"]
   }
 };
 
